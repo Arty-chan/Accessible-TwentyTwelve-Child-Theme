5fade584e2aaf7fbc4bb2c1af1f0589d77389070
Cynthia Ng <wuz_sup@hotmail.com>
2014-02-21 15:50:20 -0800

    Add IE stylesheet to support IE8 & 7

--- functions.php
+++ functions.php
@@ -12,4 +12,15 @@ add_action( 'after_setup_theme', 'childtheme_setup' );
 function childtheme_setup() {
 	require_once( get_stylesheet_directory() . '/inc/childtheme-settings.php' );
 }
+
+/**
+ * must use priority greater than 10
+ * then add our special version in child theme's css folder
+ */
+function mytheme_enqueue_styles() {
+	global $wp_styles;
+	wp_enqueue_style( 'mytheme-ie', get_stylesheet_directory_uri() . '/css/ie.css', array( 'twentytwelve-style' ), '1.0' );
+	$wp_styles->add_data( 'mytheme-ie', 'conditional', 'lt IE 9' );
+}
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_styles', 11 );
 ?>
